d9b1f755c508837a86ad72798d3ebc5e6dae99ea
leleo6 <leonardoleleoaq@gmail.com>
2026-06-01 22:26:37 -0500

    guardado rapido

--- basics/src/module/proyects/mod.rs
+++ basics/src/module/proyects/mod.rs
@@ -1,3 +1,4 @@
 pub mod cotizacion;
 pub mod p02_cotizacion;
 pub mod v01_ADSO_cotiza;
+pub mod v02_ADSO_cotiza;
--- basics/src/module/proyects/v02_ADSO_cotiza.rs
+++ basics/src/module/proyects/v02_ADSO_cotiza.rs
@@ -1,2 +1,35 @@
 use std::io;
 
+struct ProductoInformacion{
+    nombre          : String,
+    precio_unitario : f64,
+    porciones       : i32
+}
+impl ProductoInformacion{
+}
+
+fn menu() {
+    let mut menu_completo = format!{
+       "(A). Ingresar un nuevo producto\n
+        (B). Agregar Parametros Adicionales\n
+        (C). Mostrar Factura\n
+        (D). Limpiar\n
+        (E). Salir\n
+        "
+    };
+}
+
+
+
+pub fn cotizacion(){
+    loop {
+        let mut entrada_menu : String = String::new();
+        println!("!!!!Bienvenido A cotizador ADSO!!!!");
+        menu();
+        match io::stdin().read_line(&mut entrada_menu) {
+            Ok(_) => {},
+            Err(_) => {entrada_menu = "r232"},
+        }
+
+    }
+}
