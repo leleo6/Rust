05a8d0d6b8ec56a9e27dd54acf4704f54e621549
leleo6 <leonardoleleoaq@gmail.com>
2026-06-08 16:41:53 -0500

    tener una base de datos en un vector el cual almacena struct

--- basics/src/module/proyects/v02_ADSO_cotiza.rs
+++ basics/src/module/proyects/v02_ADSO_cotiza.rs
@@ -22,11 +22,30 @@ fn menu_completo() {
 
 
 
+
 pub fn cotizacion(){
+    //Creamos la entrada para los datos que da el usuario
+
     let mut entrada_menu : String = String::new();
+    let mut entrada_producto_nombre : String = String::new();
+    let mut entrada_producto_precio : String = String::new();
+    let mut entrada_producto_porciones : String = String::new();
+
+
+    //vector en el cual guardamos todos los productos 
+
+    let mut base_datos_temporal:Vec<ProductoInformacion> =Vec::new();
     println!("!!!!Bienvenido A cotizador ADSO!!!!");
+
     loop {
-        entrada_menu.clear();//limpia el contenido de la variable
+        // Limpiamos el contenido de las variables
+
+        entrada_menu.clear();
+        entrada_producto_nombre.clear();
+        entrada_producto_precio.clear();
+        entrada_producto_porciones.clear();
+
+        // Imprimimos el menu y solicitamos la opcion que desea el usuario 
 
         menu_completo();
         if let Err(e) =  io::stdin().read_line(&mut entrada_menu){
@@ -35,7 +54,41 @@ pub fn cotizacion(){
         }
         let entrada:&str = &entrada_menu.trim().to_uppercase();
         match &entrada as &str {
-            "A" => println!("funciona la obcion A"),
+            "A" => {
+
+                // Solicitamos los datos necesarios del producto
+
+                println!("Ingrese el nombre del producto");
+                if let Err(e) = io::stdin().read_line(&mut entrada_producto_nombre){
+                    eprintln!("error al recibir la entrada del menu");
+                    panic!();
+                };
+                
+
+                println!("ingrese el precio del producto");
+                if let Err(e) = io::stdin().read_line(&mut entrada_producto_precio){
+                    eprintln!("error al recibir la entrada del menu");
+                    panic!();
+                };
+
+                println!("Ingrese el numero de porciones del producto");
+                if let Err(e) = io::stdin().read_line(&mut entrada_producto_porciones){
+                    eprintln!("error al recibir la entrada del menu");
+                    panic!();
+                };
+
+                let mut entrada_base_datos : ProductoInformacion = ProductoInformacion { 
+                    nombre: entrada_producto_nombre.trim().to_string(), 
+                    precio_unitario: entrada_producto_precio.trim().parse().expect("precio unitario fallido intentelo nuevamente"), 
+                    porciones: entrada_producto_porciones.trim().parse().expect(""), 
+                };
+
+                //Ingresamos el producto a la base de datos 
+
+                base_datos_temporal.push(entrada_base_datos);
+
+
+            },
             "B" => println!("funciona la opcion B"),
             "C" => println!("funciona la opcion C"),
             "D" => println!("funciona la opcion D"),
