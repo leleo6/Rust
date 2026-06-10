b4cd384cc3b19ca760e86346c733cc93706cd886
leleo6 <leonardoleleoaq@gmail.com>
2026-06-09 21:35:43 -0500

    propiedad y comunicacion entre hilos ( manejo de variables )

--- basics/src/module/p08_simple_basics.rs
+++ basics/src/module/p08_simple_basics.rs
@@ -1,4 +1,5 @@
-use std::sync::Mutex;
+use std::sync::{Arc,Mutex};
+use std::thread;
 
 
 pub fn comunicacion_compartida(){
@@ -9,5 +10,46 @@ pub fn comunicacion_compartida(){
         *num += 1;                       // modifica el dato
     } // el lock se libera automáticamente
     println!("{:?}", m); // Mutex { data: 6 }
-    
+
+    // Arch Permite que varios hilos sean dueños compartidos del mismo dato.
+
+    let contador = Arc::new(Mutex::new(0));
+    let mut manejadores = vec![];
+
+    for _ in 0..10 {
+        let contador = Arc::clone(&contador); // clona el Arc (incrementa el contador)
+        let manejador = thread::spawn(move || {
+            let mut num = contador.lock().unwrap();
+            *num += 1;
+        });
+        manejadores.push(manejador);
+    }
+
+    for manejador in manejadores {
+        manejador.join().unwrap();
+    }
+
+    println!("Resultado final: {}", *contador.lock().unwrap());
+
+
+
+    // uso compartido
+    let contador = Arc::new(Mutex::new(0));
+    let mut manejadores = vec![];
+
+    for _ in 0..10 {
+        let contador = Arc::clone(&contador); // clona el Arc (incrementa el contador)
+        let manejador = thread::spawn(move || {
+            let mut num = contador.lock().unwrap();
+            *num += 1;
+        });
+        manejadores.push(manejador);
+    }
+
+    for manejador in manejadores {
+        manejador.join().unwrap();
+    }
+
+    println!("Resultado final: {}", *contador.lock().unwrap());
+
 }
