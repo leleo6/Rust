ea9fcd30fc6a52258b4d2c2705b5bc0fe296b894
leleo6 <leonardoleleoaq@gmail.com>
2026-06-09 21:45:43 -0500

    genericos

---
+++ basics/src/module/p09_simple_basics.rs
@@ -0,0 +1,20 @@
+
+
+/* 
+generiacos
+Basicamente son funciones que funcionan con cualquier "tipo de dato"
+funciona con funciones, structs, enums, métodos e implementaciones
+*/ 
+
+//un genericos implementado en una funcion
+
+fn mayor<T: PartialOrd>(a: T, b: T) -> T {
+    if a > b { a } else { b }
+}
+
+fn main() {
+    let entero = mayor(10, 20);
+    let flotante = mayor(3.14, 2.71);
+    let caracter = mayor('x', 'a');
+    println!("{entero}, {flotante}, {caracter}");
+}
