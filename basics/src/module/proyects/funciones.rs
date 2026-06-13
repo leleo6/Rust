bfd4ab18fee779584bfb708ce9b11993e6c26e04
leleo6 <leonardoleleoaq@gmail.com>
2026-06-13 14:42:12 -0500

    solicitud de datos completa (profundizacion)

--- basics/src/module/proyects/funciones.rs
+++ basics/src/module/proyects/funciones.rs
@@ -1,40 +1,65 @@
 pub mod funciones_utiles {
-    use std::{io, option};
+    use std::{io,};
 
-    struct cedula{
-        primer_nombre: String,
-        segundo_nombre: String,
-        apellidos: String,
-        numero_documento: String,
-        fecha_nacimiento:String,
-    }
-    impl cedula {
-        fn solicitar_datos(&self) -> cedula{
-            let datos = cedula{
-                primer_nombre:   "32".to_string(),
-                segundo_nombre:  "32".to_string(),
-                apellidos:       "32".to_string(),
-                numero_documento:"32".to_string(),
-                fecha_nacimiento:"32".to_string(),
-            };
-            let mut entrada =String::new();
-            for i in 1..5 {
-              entrada.clear();
-              match i {
-                1 => {
-                    datos.primer_nombre.clear();
-                    println!("Ingresa tu Primer nombre");
-                    match io::stdin().read_line(&mut entrada) {
-                        Ok(_) => {entrada.trim().to_string()},
-                        Err(_) => {panic!()} ,
+        pub struct cedula{
+            primer_nombre: String,
+            segundo_nombre: String,
+            apellidos: String,
+            numero_documento: String,
+            fecha_nacimiento:String,
+        }
+        impl cedula {
+            pub fn solicitar_datos(& mut self){
+                let mut entrada =String::new();
+
+                for i in 1..=5 {
+                entrada.clear();
+                match i {
+                    1 => {
+                        self.primer_nombre.clear();
+                        println!("Ingresa tu Primer nombre");
+                        match io::stdin().read_line(&mut entrada) {
+                            Ok(_) => {self.primer_nombre = entrada.trim().to_string()},
+                            Err(_) => {panic!()} ,
+                        };
+                    }
+                    2 => {
+                        self.segundo_nombre.clear();
+                        println!("Ingresa tu segundo nombre");
+                        match io::stdin().read_line(&mut entrada) {
+                            Ok(_) => {self.segundo_nombre = entrada.trim().to_string()},
+                            Err(_) => {panic!()} ,
                     };
-                    
+                    }
+                    3 => {
+                        self.apellidos.clear();
+                        println!("Ingresa tus apellidos");
+                        match io::stdin().read_line(&mut entrada) {
+                            Ok(_) => {self.apellidos = entrada.trim().to_string()},
+                            Err(_) => {panic!()} ,
+                    };
+                    }
+                    4 => {
+                        self.numero_documento.clear();
+                        println!("Ingresa tu numero de documento nombre");
+                        match io::stdin().read_line(&mut entrada) {
+                            Ok(_) => {self.numero_documento = entrada.trim().to_string()},
+                            Err(_) => {panic!()} ,
+                    };
+                    }
+                    5 => {
+                        self.fecha_nacimiento.clear();
+                        println!("Ingresa tu fecla de nacimiento nombre");
+                        match io::stdin().read_line(&mut entrada) {
+                            Ok(_) => {self.fecha_nacimiento = entrada.trim().to_string()},
+                            Err(_) => {panic!()} ,
+                    };
+                    }
 
-                }
-                  
-              }  
+                    _ => {}
+                    
+                };
             };
         }
-        
     }
 }
