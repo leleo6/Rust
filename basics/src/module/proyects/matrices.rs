855dd7b5eff6a110e521afad3cfd6e56170763bf
leleo6 <leonardoleleoaq@gmail.com>
2026-06-14 17:12:02 -0500

    profundizando vectores

---
+++ basics/src/module/proyects/matrices.rs
@@ -0,0 +1,67 @@
+
+use std::io;
+
+fn mostrar_menu(){
+    println!("
+        (A) incertar un numero
+        (B) incentar un mensaje
+        (C) Mostrar
+        (D) Salir
+    ")
+}
+
+
+pub fn meter_incices_mostar(){
+    let mut entrada =String::new();
+    let mut vector_numeros: Vec<f64> = vec![];
+    let mut vector_mensaje: Vec<f64> = vec![];
+    let mut letra:char ='0';
+
+    println!("Manejo de Vectores");
+    loop {
+        mostrar_menu();
+        if let Err(e) = io::stdin().read_line(&mut entrada) {
+            eprintln!("Error {}",e);
+            continue;
+        };
+        letra = match entrada.trim().chars().next(){
+            Some(e) => e,
+            None => {
+                println!("Error solo ingresa una letra");
+                continue;
+            }
+        };
+
+        match letra {
+            'A' => {
+                entrada.clear();
+                println!("ingrese el numero que desea ingredar");
+                if let Err(e) = io::stdin().read_line(&mut entrada){
+                    eprintln!("error al leer el numero : {}",e);
+                } 
+                if let Ok(numero) = entrada.trim().parse(){
+                    vector_numeros.push(numero);
+                } else {
+                    println!("No se ingresó un número, inténtalo nuevamente.");
+                    continue;
+                }
+            },
+            'B' => {
+                entrada.clear();
+                println!("ingrese el numero que desea ingredar");
+                if let Err(e) = io::stdin().read_line(&mut entrada){
+                    eprintln!("error al leer el numero : {}",e);
+                } 
+                if let Some(numero) = entrada.trim().to_string(){
+                    vector_numeros.push(numero);
+                } else {
+                    println!("No se ingresó un número, inténtalo nuevamente.");
+                    continue;
+                }
+            },
+            
+        };
+
+
+    }
+}
--- basics/src/module/proyects/mod.rs
+++ basics/src/module/proyects/mod.rs
@@ -4,3 +4,4 @@ pub mod v01_ADSO_cotiza;
 pub mod v02_ADSO_cotiza;
 pub mod funciones;
 pub mod vectores_vectores_matrices;
+pub mod matrices;
