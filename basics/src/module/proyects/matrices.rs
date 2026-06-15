0d29b146c55af8fc5385ab9d409600362ec96c4a
leleo6 <leonardoleleoaq@gmail.com>
2026-06-15 12:41:26 -0500

    Profundizacion vectores(Finalizada)

--- basics/src/module/proyects/matrices.rs
+++ basics/src/module/proyects/matrices.rs
@@ -14,7 +14,7 @@ fn mostrar_menu(){
 pub fn meter_incices_mostar(){
     let mut entrada =String::new();
     let mut vector_numeros: Vec<f64> = vec![];
-    let mut vector_mensaje: Vec<f64> = vec![];
+    let mut vector_mensaje: Vec<String> = vec![];
     let mut letra:char ='0';
 
     println!("Manejo de Vectores");
@@ -52,13 +52,28 @@ pub fn meter_incices_mostar(){
                 if let Err(e) = io::stdin().read_line(&mut entrada){
                     eprintln!("error al leer el numero : {}",e);
                 } 
-                if let Some(numero) = entrada.trim().to_string(){
-                    vector_numeros.push(numero);
-                } else {
-                    println!("No se ingresó un número, inténtalo nuevamente.");
+                
+                if entrada.is_empty() {
+                    println!("tiene que ingresar un valor, intentelo nuevamente mas tarde");
                     continue;
+                } else {
+                    entrada.trim().to_string();
+                    vector_mensaje.push(entrada.clone());
                 }
             },
+            'C' => {
+                println!("
+                Valores numericos almacenados: {:?}
+                mensajes almacenados: {:?}
+                ",vector_numeros,vector_mensaje);
+            },
+            'D' => {
+                println!("hasta la proxima.....");
+                break;
+            }
+            _ => {
+            println!("Opción no válida, por favor selecciona A, B, C o D.");
+            }
             
         };
 
